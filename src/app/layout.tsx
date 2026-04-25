86c8777f639f03091f2d705384e08e97488d7e0c
SANOOPKCT <snoopkct@gmail.com>
2026-04-25 15:37:32 +0530

    02: authenticaton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter } from "next/font/google";
+
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import { dark } from '@clerk/themes';
 import { ThemeProvider } from "@/components/theme-provider";
 
 import "./globals.css";
@@ -26,19 +29,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${inter.variable} ${PlexMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem={false}
-          disableTransitionOnChange
+    <ClerkProvider
+       appearance={{
+         theme: dark,
+       }}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${inter.variable} ${PlexMono.variable} antialiased`}
         >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            disableTransitionOnChange
+          >
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-rose-500 text-white p-2 rounded">Sign Up</button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
